e55c1bbf2b3e761faef2dbc30a629b5442b585c0
ZakK <ZakK@192.168.0.101>
2019-12-01 19:12:00 +0200

    Старт1

---
+++ KOE_Print/src/main/java/KOE_Print/App.java
@@ -0,0 +1,13 @@
+package KOE_Print;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
